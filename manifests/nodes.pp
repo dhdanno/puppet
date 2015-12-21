96dcff4810a89114739a39061ecd0d0fff202d21
Danno <dhdanno@DMac.local>
2015-12-22 07:45:16 +0800

    changes

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -5,9 +5,24 @@ node default {
     include postgres
     include git
 
+    file { '/usr/local/app':
+	    ensure => directory,
+	    owner => 'bob',
+	    group => 'bob',
+	    mode => 755,
+	}
+
     git::clone { '<GitHub repository name>':
 	    path => '/usr/local/app',
 	    dir => 'django',
 	}
 
 }
+
+
+    file { '/usr/local/bob':
+	    ensure => directory,
+	    owner => 'bob',
+	    group => 'bob',
+	    mode => 755,
+	}
